60a06441fb40c38c29f4832d174138329f709362
Shashwat Tiwari <shashwat.monphy@gmail.com>
2024-08-08 15:48:10 +0530

    final commit

--- index.js
+++ index.js
@@ -1,43 +1,48 @@
-const app = require("express")();
-const server = require("http").createServer(app);
-const cors = require("cors");
+const app = require('express')();
+const server = require('http').createServer(app);
+const cors = require('cors');
 
-const io = require("socket.io")(server, {
+const io = require('socket.io')(server, {
 	cors: {
-		origin: "*",
-		methods: ["GET", "POST"],
-	},
+		origin: '*',
+		methods: ['GET', 'POST']
+	}
 });
 
 app.use(cors());
 
-const PORT = process.env.PORT || 5000;
+const users = {};
 
-let users = [];
+io.on('connection', (socket) => {
+	socket.emit('me', socket.id);
 
-app.get('/', (req, res) => {
-	res.send('Running');
+	socket.on('join', ({ name }) => {
+		users[socket.id] = { id: socket.id, name };
+
+		io.emit('updateUserList', Object.values(users));
+	});
+
+	socket.on('callUser', ({ userToCall, signalData, from, name }) => {
+		io.to(userToCall).emit('callUser', { signal: signalData, from, name });
+	});
+
+	socket.on('answerCall', (data) => {
+		io.to(data.to).emit('callAccepted', data.signal);
+	});
+
+	socket.on('callEnded', () => {
+		delete users[socket.id];
+		io.emit('updateUserList', Object.values(users));
+	});
+
+	socket.on('disconnect', () => {
+		delete users[socket.id];
+		io.emit('updateUserList', Object.values(users));
+	});
+
+	socket.on('sendMessage', ({ to, message, from }) => {
+		io.to(to).emit('receiveMessage', { message, from });
+	});
 });
 
-io.on("connection", (socket) => {
-	socket.emit("me", socket.id);
-
-	users.push({ id: socket.id });
-	io.emit("updateUserList", users);
-
-	socket.on("disconnect", () => {
-		users = users.filter(user => user.id !== socket.id);
-		io.emit("updateUserList", users);
-		socket.broadcast.emit("callEnded");
-	});
-
-	socket.on("callUser", ({ userToCall, signalData, from, name }) => {
-		io.to(userToCall).emit("callUser", { signal: signalData, from, name });
-	});
-
-	socket.on("answerCall", (data) => {
-		io.to(data.to).emit("callAccepted", data.signal);
-	});
-});
-
-server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+server.listen(5000, () => console.log('Server is running on port 5000'));
